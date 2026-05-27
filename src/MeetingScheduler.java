264679efadce028df324a4677a2eaadbbb0c1302
Morten Clausen <morten.t.clausen@hotmail.com>
2026-05-27 18:12:25 +0200

    finished first implementation of each method

--- src/MeetingScheduler.java
+++ src/MeetingScheduler.java
@@ -1,12 +1,13 @@
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.SortedSet;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.stream.Stream;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -64,8 +65,7 @@ public class MeetingScheduler implements Scheduler {
         meetings.remove(idx);
     }
 
-    @Override
-    public Meeting CreateMeeting(Collection<Person> people, Calendar timeslot) throws RuntimeException {
+    private void validateTimeslot(Calendar timeslot){
         int
         hour = timeslot.get(Calendar.HOUR_OF_DAY),
         minute = timeslot.get(Calendar.MINUTE),
@@ -74,6 +74,11 @@ public class MeetingScheduler implements Scheduler {
             throw new IllegalArgumentException(
                 String.format("Meetings must start on the hour. Time given: %d.2:%d.2:%d.2", hour, minute, second)
             );
+    }
+
+    @Override
+    public Meeting CreateMeeting(Collection<Person> people, Calendar timeslot) throws RuntimeException {
+        validateTimeslot(timeslot);
         people.forEach(p -> validateEmail(p.email(), true));
 
         Meeting m = new Meeting(timeslot, people);
@@ -103,9 +108,55 @@ public class MeetingScheduler implements Scheduler {
     }
 
     @Override
-    public Stream<Date> SuggestTimeslot(Collection<String> emails) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'SuggestTimeslot'");
+    public Stream<Calendar> SuggestTimeslot(Collection<String> emails) {
+        Calendar now = Calendar.getInstance(), end = Calendar.getInstance();
+        end.add(Calendar.WEEK_OF_YEAR, 1);
+        return SuggestTimeslot(emails, now, end);
+    }
+
+    private void adjustTime(Calendar timeslot, int shift){
+        int
+        hour = timeslot.get(Calendar.HOUR_OF_DAY),
+        date = timeslot.get(Calendar.DATE),
+        month = timeslot.get(Calendar.MONTH),
+        year = timeslot.get(Calendar.YEAR);
+        timeslot.set(year, month, date, hour, 0, 0);
+        timeslot.add(Calendar.HOUR_OF_DAY, shift);
+    }
+
+    private Stream<Calendar> subspan(String email, Calendar after, Calendar before){
+        List<UUID> meetings = peopleMap.get(email);
+        UUID dummy1 = UUID.randomUUID(), dummy2 = UUID.randomUUID();
+        meetingMap.put(dummy1, new Meeting(after, new ArrayList<>()));
+        meetingMap.put(dummy2, new Meeting(before, new ArrayList<>()));
+        int startIdx = Collections.binarySearch(meetings, dummy1);
+        int endIdx = Collections.binarySearch(meetings, dummy2);
+        startIdx = (startIdx < 0) ? -(startIdx+1) : startIdx;
+        endIdx = (endIdx < 0) ? -(endIdx+1) : endIdx;
+        meetingMap.remove(dummy1); meetingMap.remove(dummy2);
+        return
+            meetings
+            .subList(startIdx, endIdx)
+            .parallelStream()
+            .map(id -> meetingMap.get(id).timeslot());
+    }
+
+    @Override
+    public Stream<Calendar> SuggestTimeslot(Collection<String> emails, Calendar startAfter, Calendar endBefore) {
+        emails.forEach(e -> validateEmail(e, true));
+        adjustTime(startAfter, 1); adjustTime(endBefore, -1);
+        if(startAfter.compareTo(endBefore) >= 0)
+            throw new IllegalArgumentException("'endBefore' must indicate a time at least 2 hours later than 'startAfter'.");
+        
+        SortedSet<Calendar> suggestions = new ConcurrentSkipListSet<>();
+        while(startAfter.before(endBefore)){
+            suggestions.add((Calendar)startAfter.clone());
+            startAfter.add(Calendar.HOUR_OF_DAY, 1);
+        }
+
+        emails.parallelStream().forEach(e -> subspan(e, startAfter, endBefore).forEach(suggestions::remove));
+
+        return suggestions.stream();
     }
     
 }
--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,13 +1,12 @@
 import java.util.Calendar;
 import java.util.Collection;
-import java.util.Date;
-import java.util.List;
 import java.util.stream.Stream;
 
 public interface Scheduler {
-    public Person CreatePerson(String name, String email) throws RuntimeException;
-    public Meeting CreateMeeting(Collection<Person> people, Calendar timeslot) throws RuntimeException;
+    public Person CreatePerson(String name, String email) throws IllegalArgumentException;
+    public Meeting CreateMeeting(Collection<Person> people, Calendar timeslot) throws IllegalArgumentException;
     public Stream<Meeting> ShowSchedule(String email);
     public Stream<Meeting> ShowSchedule(Person person);
-    public Stream<Date> SuggestTimeslot(Collection<String> emails);
+    public Stream<Calendar> SuggestTimeslot(Collection<String> emails);
+    public Stream<Calendar> SuggestTimeslot(Collection<String> emails, Calendar after, Calendar before);
 }
